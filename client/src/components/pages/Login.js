0d180a2fbb4af6bc8f325a7fc986a925a5a8fda0
TaoguangXing <taoguangxing@gmail.com>
2021-10-14 19:18:55 -0400

    Changed some UI

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,31 +1,31 @@
 // Import dependencies
-import React, { Component } from "react";
+import React from "react";
 import Button from 'react-bootstrap/button';
 import { Col } from 'react-bootstrap';
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <div className="outer">
-                <form className="inner">
-                    <h3>Log In</h3>
+function Login() {
+    return (
+        <div className="outer">
+            <form className="inner">
+                <h3>Log In</h3>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-                    <Button as={Col} variant="success" size='xs' xs={{ span: 12 }} className="form-button">Log In</Button>
-                    <p className="forgot-password text-right">
-                        Forgot your password? <a href="/login">Click here</a>
-                    </p>
-                </form>
-            </div>
-        );
-    }
+                <Button as={Col} variant="success" size='xs' xs={{ span: 12 }} className="form-button">Log In</Button>
+                <p className="forgot-password text-right">
+                    Forgot your password? <a href="/login">Click here</a>
+                </p>
+            </form>
+        </div>
+    );
 }
+
+export default Login;
--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from 'react-bootstrap/button';
 import { Col } from 'react-bootstrap';
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <div className="outer">
-                <form className="inner">
-                    <h3>Sign Up</h3>
+function Signup() {
+    return (
+        <div className="outer">
+            <form className="inner">
+                <h3>Sign Up</h3>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">First name</label>
-                        <input type="text" className="form-control" placeholder="First name" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Last name</label>
-                        <input type="text" className="form-control" placeholder="Last name" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-                    <div className="form-group">
-                        <label className="custom-control-label">Confirm password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
+                <div className="form-group">
+                    <label className="custom-control-label">Confirm password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-                    <Button as={Col} variant="success" size='xs' xs={{ span: 12 }} className="form-button">Sign Up</Button>
-                    <p className="forgot-password text-right">
-                        Already registered <a href="/login">log in?</a>
-                    </p>
-                </form>
-            </div>
-        );
-    }
+                <Button as={Col} variant="success" size='xs' xs={{ span: 12 }} className="form-button">Sign Up</Button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="/login">log in?</a>
+                </p>
+            </form>
+        </div>
+    );
 }
+
+export default Signup;
